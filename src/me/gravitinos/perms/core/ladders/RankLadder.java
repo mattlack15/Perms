0846ba031878f9e2e1df2439633497ae6c98d69b
BuildTools <unconfigured@null.spigotmc.org>
2020-05-18 21:23:55 +0200

    Added RankLadder

--- src/me/gravitinos/perms/core/ladders/RankLadder.java
+++ src/me/gravitinos/perms/core/ladders/RankLadder.java
@@ -1,4 +1,23 @@
 package me.gravitinos.perms.core.ladders;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import me.gravitinos.perms.core.group.Group;
+import me.gravitinos.perms.core.user.User;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
 public class RankLadder {
+    private String name;
+    private List<Group> groups;
+
+    public void setRank(User user, int index) {
+
+    }
+
+    public void promote(User user) {
+
+    }
 }
